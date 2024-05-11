26049ec49bae9779e29872a56973d7b7819396cf
serPeterDuenas <serpeterduenas@gmail.com>
2024-05-11 00:14:48 -0700

    working on UI

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -5,14 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // This class will hold the Player's lives
-    // This class will know when to reload the scene
     private static GameManager thisInstance;
-   
 
     // Start is called before the first frame update
     void Start()
     {
+       
         DontDestroyOnLoad(gameObject);
         if (thisInstance == null)
         {
@@ -39,6 +37,5 @@ public class GameManager : MonoBehaviour
         //Debug.Log("Respawning player");
         var scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
-        player.transform.position = checkpointLocation.transform.position;
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,22 +7,6 @@ using static UnityEngine.GraphicsBuffer;
 public class Player : MonoBehaviour
 {
 
-    //[Header("Movement controls")]// Player's movement controls
-    //[SerializeField] private float moveSpeed = 15f;
-    //[SerializeField] private float acceleration = 50f;
-    //[SerializeField] private int jumpPower = 15;
-    //[SerializeField] private float jumpResistance = 10f;
-    //public Transform groundCheck;
-    //public LayerMask groundLayer;
-    ////[SerializeField] private bool spawnFacingLeft = false;
-    //private bool isFacingRight;
-    //private float moveX;
-    //private int moveDirection;
-    //private Rigidbody2D rb;
-    //// Player's logic for moving platform
-    //public bool isOnPlatform = false;
-    //public Rigidbody2D platformRB;
-
     [Header("Health attributes")]
     [SerializeField] private HealthBar healthBar;
     [SerializeField] private int maxHealth = 100;
@@ -31,28 +15,18 @@ public class Player : MonoBehaviour
     private int currentLives;
 
 
-    [SerializeField] private LevelManager levelManager;
+    //[SerializeField] private LevelManager levelManager;
     [SerializeField] private LivesManager livesManager;
     private bool isDead = false;
-
-
-    //[Header("Weapon attributes")]
-    //public Transform weapon;
-    //public LayerMask enemyLayerCheck;
-    //[SerializeField] private float attackRange = 0.5f;
-    //[SerializeField] private int attackDamage = 10;
-
 
     
 
 
     public void TakeDamage(int damageValue)
     {
-        //Debug.Log("I took damage -- player");
         CurrentHealth -= damageValue;
         healthBar.SetHealth(CurrentHealth);
-        //Debug.Log("I have this much health: ");
-        //Debug.Log(CurrentHealth);
+
 
         if (CurrentHealth <= 0 )
         {
@@ -65,7 +39,7 @@ public class Player : MonoBehaviour
         healthBar = FindObjectOfType<HealthBar>();
         CurrentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
-        levelManager = FindObjectOfType<LevelManager>();
+        //levelManager = FindObjectOfType<LevelManager>();
         livesManager = FindObjectOfType<LivesManager>();
         currentLives = totalLives;
 
@@ -87,6 +61,8 @@ public class Player : MonoBehaviour
         CurrentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         livesManager.UpdateLives(takeAwayLife);
+        var scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 
    
