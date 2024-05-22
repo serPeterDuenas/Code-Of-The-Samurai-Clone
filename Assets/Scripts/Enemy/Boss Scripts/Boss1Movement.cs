847a1b935518277ac1db7880fef8a7e39f2fb990
CarloZep <carlogab.zepeda@gmail.com>
2024-05-21 18:29:23 -0700

    testies

--- Assets/Scripts/Enemy/Boss Scripts/Boss1Movement.cs
+++ Assets/Scripts/Enemy/Boss Scripts/Boss1Movement.cs
@@ -44,20 +44,10 @@ public class Boss1Movement : MonoBehaviour
 
     private void DashInDirection(int _direction)
     {
-        waitOnDashCooldown = 0;
-        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction,
-            initScale.y, initScale.z);
-
-        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
-            enemy.position.y, enemy.position.z);
     }
 
 
     private void DirectionChange()
     {
-        waitOnDashCooldown += Time.deltaTime;
-
-        if (idleTimer > idleDuration)
-            movingLeft = !movingLeft;
     }
 }
